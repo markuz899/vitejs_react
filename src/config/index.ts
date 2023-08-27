602f0cf4db9254517af8e4a0cc114dc1aec4ee60
markuz899 <markuz.89@hotmail.it>
2023-08-27 21:59:19 +0200

    add info in json

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,10 @@
 export const userInfo = {
+  picture: "",
   name: "Marco",
   surname: "Liberati",
   fullname: "Marco Liberati",
+  email: "marcoliberati.89@gmail.com",
+  cvFile: "marco_liberati.csv",
   birtday: "06/07/1989",
   role: "Web developer",
   funFact: "Take care of your life, develop prudently",
@@ -19,8 +22,41 @@ export const userInfo = {
     ],
   },
   information: {
+    available: false,
     location: "Italy",
     experience: "2019",
+    availableFor: "Piccoli progetti",
     relocation: "No",
+    aboutMe: {
+      info: "Libero quas veritatis nulla distinctio fuga nihil temporibus et. Quia dicta sapiente qui porro molestiae nobis incidunt voluptatem. Et voluptas sunt nihil. At perferendis voluptatem dolores nulla. Adipisci dolore non. Praesentium ipsa magnam ut quia explicabo voluptates.",
+      github: "https://github.com/markuz899",
+      linkedin: "https://www.linkedin.com/in/marco-liberati-010302194/",
+    },
   },
+  experience: [
+    {
+      role: "Web developer - Backend",
+      agency: "Azienda",
+      location: "Italia",
+    },
+    {
+      role: "Web developer - Frontend",
+      agency: "Azienda",
+      location: "Italia",
+    },
+  ],
+  project: [
+    {
+      type: "Type of project",
+      name: "Project name",
+      description: "Description project",
+    },
+  ],
+  education: [
+    {
+      type: "ITIS",
+      name: "Enrico Fermi",
+      location: "Roma",
+    },
+  ],
 };
