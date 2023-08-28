dd6e3486f42f32f24a3f177ee9d02c5669669359
markuz899 <markuz.89@hotmail.it>
2023-08-28 14:37:52 +0200

    [game] - puzzle

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,12 @@
+export const images = [
+  "https://i.etsystatic.com/23997006/r/il/d7a0cb/3693314820/il_340x270.3693314820_ph5n.jpg",
+  "https://tecnogazzetta.it/wp-content/uploads/2022/08/Bing-Sula-Nicky-TecnoGazzetta-Disney-Plus-Streaming-2022-930x581.jpg",
+  "https://www.tvzoom.it/wp-content/uploads/2022/03/cocomelon.jpeg",
+  "https://www.varesenews.it/photogallery_new/images/2016/12/masha-e-orso-584781.610x431.jpg",
+  "https://cinemaserietv.it/wp-content/uploads/2022/09/Teletubbies.jpeg",
+  "https://target.scene7.com/is/image/Target/GUEST_324033db-691e-40bc-858b-89243f10f149?wid=668&qlt=80&fmt=pjpeg",
+];
+
 export const userInfo = {
   mainCard: {
     picture: "",
